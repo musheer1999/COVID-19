c5c4fe5c1a8050d8e99ca68ebd7dfb342ca912cc
musheer1999 <musheerahmad.710@gmail.com>
2020-05-09 02:12:53 +0530

    Updates

---
+++ precache-manifest.44fd7d3612c74401823d29053b8005e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f26271596f932e1bf5fe360882b9de0",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "b7199d35dc681eafe7a8",
+    "url": "/COVID-19/static/css/main.389ae79e.chunk.css"
+  },
+  {
+    "revision": "26d98145c63f97a43810",
+    "url": "/COVID-19/static/js/2.2dba0d60.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19/static/js/2.2dba0d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7199d35dc681eafe7a8",
+    "url": "/COVID-19/static/js/main.4b685a5b.chunk.js"
+  },
+  {
+    "revision": "cabebdbb08d030cce81e",
+    "url": "/COVID-19/static/js/runtime-main.e230c636.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19/static/media/image.d7265326.png"
+  }
+]);
